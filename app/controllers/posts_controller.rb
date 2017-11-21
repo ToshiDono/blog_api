71e06c59716f8b69d6a16e113491ac9e325737a5
Anton <anton@ondoc.me>
2017-11-22 00:35:08 +0300

    Create PostsController#create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+  def create
+    @post = PostCreator.run(post_params)
+    if @post.valid?
+      render json: @post, status: 200
+    else
+      render json: @post, status: 422
+    end
+  end
+
+  private
+
+  def post_params
+    params.permit(:login, post: [:title, :body, :author_ip])
+  end
 end
