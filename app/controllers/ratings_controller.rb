eef9ee53fc02eb2d2bec4f0ad8e429e2ef56f1e4
Anton <anton@ondoc.me>
2017-11-23 10:57:49 +0300

    Create RatingsController#create

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,16 @@
 class RatingsController < ApplicationController
+  def create
+    @rating = RatingCreator.new(rating_params).average_rating
+    if @rating
+      render json: @rating, status: 200
+    else
+      render json: @rating, status: 422
+    end
+  end
+
+  private
+
+  def rating_params
+    params.permit(rating: [:post_id, :estimation])
+  end
 end
