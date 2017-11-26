5b9e6178c80074319cd77c86f583d91f05514874
Anton <anton@ondoc.me>
2017-11-26 23:13:56 +0300

    Update ratings seed

--- db/seeds/3_ratings.seed.rb
+++ db/seeds/3_ratings.seed.rb
@@ -1,6 +1,7 @@
 4000.times do
-  Rating.create!(
-      estimation: rand(1..5),
-      post_id: rand(1..200)
-  )
+  RatingCreator.new(
+      rating: {
+          estimation: rand(1..5),
+          post_id: rand(1..200)
+      }).rating
 end
